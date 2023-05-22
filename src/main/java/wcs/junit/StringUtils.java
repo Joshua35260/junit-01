e53e8937abac0de48c1b51f6beb743fd2b4d0974
Joshua Dupin <joshua35260@gmail.com>
2023-05-22 15:37:36 +0200

    quest 01 done

---
+++ src/main/java/wcs/junit/StringUtils.java
@@ -0,0 +1,25 @@
+package wcs.junit;
+
+public class StringUtils {
+
+    private static final String ALL_VOWELS = "aeiouyAEIOUY";
+
+    /**
+     * Renvoie la chaîne formée par les voyelles d'une chaîne de caractères
+     * @return Chaîne avec uniquement des voyelles
+     */
+    public static String vowels(String candidate) {
+        if (candidate == null) {
+            throw new IllegalArgumentException("La chaîne de caractères ne peut pas être nulle");
+        }
+        
+        String vowels = "";
+        char[] letters = candidate.toCharArray();
+        for (int i = 0; i < candidate.length(); i++) {
+            if (ALL_VOWELS.indexOf(letters[i]) >= 0) {
+                vowels += letters[i];
+            }
+        }
+        return vowels;
+    }
+}
---
+++ src/test/java/wcs/junit/StringUtilsTest.java
@@ -0,0 +1,28 @@
+package wcs.junit;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StringUtilsTest {
+  @Test
+    public void testVowels() {
+        // Cas non passant : la méthode ne remplit pas son contrat
+
+        // Cas de test 1 : chaîne de caractères vide
+        String result1 = StringUtils.vowels("");
+        Assert.assertEquals("", result1);
+
+        // Cas de test 2 : chaîne de caractères sans voyelles
+        String result2 = StringUtils.vowels("Hll WRLD");
+        Assert.assertEquals("", result2);
+
+        // Cas de test 3 : chaîne de caractères avec des voyelles
+        String result3 = StringUtils.vowels("Hello World");
+        Assert.assertEquals("eoo", result3);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testVowelsException() {
+        // Cas d'exception : paramètre en entrée non défini (null)
+        StringUtils.vowels(null);
+    }
+
+}
